719d46a85cab8040864f1a965a0379c36406ea5e
luis Colina <colina.1990lz@gmail.com>
2024-02-08 05:25:28 -0400

    tailwind project setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    colors: {
+      'rayBlue' : '#345FF6',
+      'gunmetal' : '#253347',
+      'electricBlue' : '#5E6E85',
+      'borderBlue' : '#D8E2E7'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
